0c595d0bb37e9f56924e88f63d7358153e57bba0
Tyler <ty820956@dal.ca>
2018-03-18 13:45:31 -0300

    UI navigation update
    - Tyler and Zhixi

--- app/src/main/java/com/example/csci3130_baseproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130_baseproject/MainActivity.java
@@ -27,6 +27,37 @@ public class MainActivity extends AppCompatActivity {
         ((TextView)findViewById((R.id.courseInfo))).setText(testCourse.viewCourseInfo());
     }
 
+    public void enterScience(View v) {
+        setContentView(R.layout.sci);
+    }
+
+    public void enterEngineer(View v) {
+        setContentView(R.layout.eng);
+    }
+
+    public void enterOthers(View v){
+        setContentView(R.layout.others);
+    }
+
+    public  void enterbande(View v)   {
+        setContentView(R.layout.bande);
+    }
+    public void displayCourseMain(View v) {
+        setContentView(R.layout.displaycoursesmain);
+    }
+
+    public void displayDailySchedule(View v) {
+        setContentView(R.layout.daily_schedule_activity);
+    }
+
+    public void enterMyCourses(View v) {
+        setContentView(R.layout.mycourses_activity);
+    }
+
+    public void enterLogout(View v) {
+        setContentView(R.layout.logout_activity);
+    }
+
     public void backToHome(View v) {
         setContentView(R.layout.activity_main);
     }
--- app/src/main/java/com/example/csci3130_baseproject/dailyScheduleActivity.java
+++ app/src/main/java/com/example/csci3130_baseproject/dailyScheduleActivity.java
@@ -2,6 +2,7 @@ package com.example.csci3130_baseproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by GhadiKashgari on 2018-03-14.
@@ -12,5 +13,9 @@ public class dailyScheduleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daily_schedule_activity);
+    }
+
+    public void backToHome(View v) {
+        setContentView(R.layout.activity_main);
     }
 }
